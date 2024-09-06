721c450b9d8721ad685515e2523a42b41be36345
Mohsinali Datardi <datardimohsinali@gmail.com>
2024-09-06 07:29:46 +0530

    Here is all components of blog management system

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBu9feth-LXGz8G6dxdv2NWcB7vDgk0x38",
+  authDomain: "blog-app-960eb.firebaseapp.com",
+  projectId: "blog-app-960eb",
+  storageBucket: "blog-app-960eb.appspot.com",
+  messagingSenderId: "986279668849",
+  appId: "1:986279668849:web:fb1128a71b5ca4ea6b7fd0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
